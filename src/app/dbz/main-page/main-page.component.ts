e0e8da682440baa2ea7efc7cf3cbc3b90811f7c2
luigialejandro <50028217+luigialejandro@users.noreply.github.com>
2023-01-23 17:14:11 -0600

    Implementing some logic to the agregar method. First comes a validation to see if the name is not empty. Then, if the user has typed a new character it will be attached to the personajes list, originally composed of two characters, Goku and Vegeta. And last, once the user submits the info of the new character then the text boxes will be cleaned (the name set empty and the power set to 0).

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -11,13 +11,37 @@ interface Personaje {
 })
 export class MainPageComponent {
 
+  personajes: Personaje[] = [
+    {
+      nombre: 'Gokú',
+      poder: 15000
+    },
+    {
+      nombre: 'Vegeta',
+      poder: 7500,
+    }
+  ];
+
   nuevo: Personaje = {
-    nombre: 'Trunks',
-    poder: 14000  
+    nombre: '',
+    poder: 0  
   }
 
   agregar( ) {
-    console.log(this.nuevo)
+
+    if ( this.nuevo.nombre.trim().length === 0 ) {
+      return;
+    }
+
+    console.log(this.nuevo);
+
+    this.personajes.push( this.nuevo );
+
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    }
+
   }
 
 }
