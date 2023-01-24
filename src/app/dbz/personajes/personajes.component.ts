e1e85042614826af4a3e31d5602adc6f3613df9c
luigialejandro <50028217+luigialejandro@users.noreply.github.com>
2023-01-23 19:25:46 -0600

    i) Communicating information from a parent component to its child component. In particular, I needed to transport personajes array from the mainpage component (parent) to the personajes component (child). I achieve this by using Input decorator and specific syntax within the app personajes selector. ii)Generating dbz dot interface dot ts file so as to save the Personaje interface.

--- src/app/dbz/personajes/personajes.component.ts
+++ src/app/dbz/personajes/personajes.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-personajes',
@@ -6,6 +6,6 @@ import { Component } from '@angular/core';
 })
 export class PersonajesComponent {
 
-  personajes: any[] = [];
+  @Input() personajes: any[] = [];
 
 }
