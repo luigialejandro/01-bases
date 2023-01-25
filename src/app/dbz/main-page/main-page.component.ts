e59b1a21d992dd88b08cfd82e2a76acb21d9ca59
luigialejandro <50028217+luigialejandro@users.noreply.github.com>
2023-01-24 19:38:19 -0600

    Creating a service (manually) by generating a new dbz dot service dot ts file and using there Injectable decorator to transform our DbzService class into a service. For the time being, a message of initialized service will be shown through the class builder. I have also put this new service in the providers list at the dbz dot module dot ts and finally, I called this service from the MainPage and the PersonajesComponent by using dependency injection.

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { Personaje } from '../interfaces/dbz.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-main-page',
@@ -27,5 +28,7 @@ export class MainPageComponent {
   agregarNuevoPersonaje( argumento: Personaje ) {
     this.personajes.push( argumento )
   }
+
+  constructor( private dbzService: DbzService ) {}
 
 }
